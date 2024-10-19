9cf9bd0755c354223a3e5a892f3ce8e3a19ee6f0
joaoestreano <joaovictorestreano@gmail.com>
2024-10-18 22:17:04 -0300

    Create Aula 9.sql

---
+++ Aula 9.sql
@@ -0,0 +1,18 @@
+--Exercicio 1
+insert into tbaluno (id, nome)
+values
+(1,'Ana Maria'),
+(2,'Luiz Carlos'),
+(3,'Maria Silva'),
+(4,'Renato Rodrigues'),
+(5,'Carla Santiago')
+
+--Exercicio 2
+insert into tbdisciplina (id, nome, carga)
+values
+(1,'Modelagem de Banco de Dados',80),
+(2,'Design Digital',80),
+(3,'Estrutura de Dados',null),
+(4,'Experiência do Usuário',40),
+(5,'Aprendizagem de Máquina',null),
+(6,'Inglês I',null)
