17830f081068d04654c68859bcb716843609bb59
joaoestreano <joaovictorestreano@gmail.com>
2024-10-18 21:40:47 -0300

    Update Aula 8.sql

--- Aula 8.sql
+++ Aula 8.sql
@@ -22,3 +22,48 @@ SELECT distinct t.nome as "Turno"
 from tbcurso as c,tbturno as t, tbcurso_por_unidade as cpu
 where t.id = cpu.idturno and c.id = cpu.idcurso 
 and c.nome = 'Gestão Financeira' order by t.nome;
+
+--Exercicio 5
+select t.nome as "Turno", count(c.id) as "Quantidade"
+from tbturno as t, tbcurso_por_unidade as cpu, tbcurso as c 
+where t.id = cpu.idturno and c.id = cpu.idcurso
+group by "Turno"
+order by "Turno";
+
+--Exercicio 6
+select t.nome as "turno", sum(vaga) as "vagas"
+from tbturno as t, tbcurso_por_unidade as cpu
+where t.id = cpu.idturno
+group by "turno"
+order by "turno";
+
+--Exercicio 7
+select unidade as "Unidade", t.nome as "Turno"
+from tbturno as t, tbcurso_por_unidade as cpu, tbunidade as u
+where t.id = cpu.idturno and u.id = cpu.idunidade
+group by "Unidade","Turno"
+order by "Unidade";
+
+--Exercicio 8
+select unidade as "Unidade", t.nome as "Turno", count (cpu.idcurso) as "Cursos"
+from tbturno as t, tbcurso_por_unidade as cpu, tbunidade as u, tbcurso as c
+where t.id = cpu.idturno and u.id = cpu.idunidade and c.id = cpu.idcurso
+group by "Unidade","Turno"
+order by "Unidade";
+
+--Exercicio 9
+select unidade as "Unidade", t.nome as "Turno", count (cpu.idcurso) as "Cursos"
+from tbturno as t, tbcurso_por_unidade as cpu, tbunidade as u, tbcurso as c
+where t.id = cpu.idturno and u.id = cpu.idunidade and c.id = cpu.idcurso
+group by "Unidade","Turno"
+having count (cpu.idcurso)=5
+order by "Unidade";
+
+--Exercicio 10
+select unidade as "Unidade", t.nome as "Turno", count (cpu.idcurso) as "Cursos"
+from tbturno as t, tbcurso_por_unidade as cpu, tbunidade as u, tbcurso as c
+where t.id = cpu.idturno and u.id = cpu.idunidade and c.id = cpu.idcurso 
+and t.nome = 'matutino' 
+group by "Unidade","Turno"
+having count (cpu.idcurso)=5
+order by "Unidade";
